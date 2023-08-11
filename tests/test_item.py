f7fb746736943b3ead27adf628819144855243d1
Veronika Surkova <surkova_nika92@mail.ru>
2023-08-12 01:30:59 +0300

    add tests to test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,19 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def item_instance():
+    return Item("Смартфон", 10000, 20)
+
+
+def test_calculate_total_price(item_instance):
+    assert item_instance.calculate_total_price() == 200000
+
+
+def test_apply_discount(item_instance):
+    item_instance.pay_rate = 0.8
+    item_instance.apply_discount()
+    assert 10000 * item_instance.pay_rate == item_instance.price
+    assert item_instance.price == 8000
